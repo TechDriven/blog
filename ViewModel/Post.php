96d2148e216b9ff12cc0497e1d25ed614d62f39f
Sanchit <sanchit@outlook.in>
2023-03-09 23:12:25 +0530

    ViewModel Implemented

--- ViewModel/Post.php
+++ ViewModel/Post.php
@@ -2,13 +2,18 @@
 
 namespace Techdriven\Blog\ViewModel;
 
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Techdriven\Blog\Api\Data\PostInterface;
+use Techdriven\Blog\Api\PostRepositoryInterface;
 use Techdriven\Blog\Model\ResourceModel\Post\Collection as PostCollection;
 
 class Post implements ArgumentInterface
 {
     public function __construct(
-        private PostCollection $postCollection
+        private PostCollection $postCollection,
+        private PostRepositoryInterface $postRepository,
+        private RequestInterface $request
     ){}
 
     public function getList()
@@ -19,6 +24,12 @@ class Post implements ArgumentInterface
     public function getCount(): int
     {
         return $this->postCollection->count();
+    }
+
+    public function getDetail(): PostInterface
+    {
+        $id = (int) $this->request->getParam('id');
+        return $this->postRepository->getById($id);
     }
 }
 
